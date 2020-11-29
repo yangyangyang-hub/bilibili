d2570b8cca71dbd3e8bdd1e0ecbd0d070d70c9d7
yangyangyang-hub <962369014@qq.com>
2020-11-29 16:35:41 +0800

    用户中心css样式

---
+++ bilibili/src/components/userComponent/userDetail.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="user_detail">
+      <div>
+          <div class="user_img">
+            <img src="../../assets/default_img.jpg" alt="">
+          </div>
+            <div class="user_edit">
+                <div>
+                    <p>
+                        <span>0</span>
+                        <span class="user_text">粉丝</span>
+                    </p>
+                    <p>
+                        <span>54</span>
+                        <span class="user_text">关注</span>
+                    </p>
+                    <p>
+                        <span>0</span>
+                        <span class="user_text">获赞</span>
+                    </p>
+                </div>
+                <div>
+                    <div class="user_editBtn">编辑资料</div>
+                </div>
+            </div>
+      </div>
+      <div>
+          <h2>小风_</h2>
+          <p>这个人很神秘，什么都没有写</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+    .user_detail {
+        background-color: white;
+        padding: 0 3.333vw;
+        >div:nth-child(1) {
+            display: flex;
+                    .user_img {
+            margin-right: 5.556vw;
+            img {
+                height: 23.611vw;
+                width: 23.611vw;
+                border-radius: 50%;
+            }
+        }
+        .user_edit {
+            flex: 1;
+            div:nth-child(1){
+                display: flex;
+                p {
+                    flex: 1;
+                    display: flex;
+                    margin: 13px 0;
+                    justify-content: center;
+                    align-items: center;
+                    flex-direction: column;
+                    font-size: 14px;
+                    .user_text {
+                        color: #aaa;
+                    }
+                }
+                p:nth-child(1),p:nth-child(2) {
+                    border-right: 1px solid #ccc;;
+                }
+            }
+            div:nth-child(2) {
+                padding: 0.556vw 2.778vw;
+                .user_editBtn {
+                    border: 0.278vw solid #fb7a9f;
+                    padding: 0.833vw;
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    color: #fb7a9f;
+                    border-radius: 1.111vw;
+                }
+            }
+        }
+        }
+        >div:nth-child(2) {
+            h2 {
+                margin: 2.778vw 0 0.833vw 0;
+                font-weight: 400;
+            }
+            p {
+                padding: 0;
+                margin: 2.778vw 0;
+                color: #999;
+            }
+        }
+
+    }
+</style>
--- bilibili/src/views/userinfo.vue
+++ bilibili/src/views/userinfo.vue
@@ -1,18 +1,28 @@
 <template>
-  <div>
+  <div class="userinfo">
       <nav-bar></nav-bar>
+      <img src="../assets/bannerTop_new.png" alt="" class="backimg">
+      <user-detail></user-detail>
   </div>
 </template>
 
 <script>
 import NavBar from '../components/common/Navbar.vue'
+import userDetail from '../components/userComponent/userDetail.vue'
 export default {
     components: {
-        NavBar
+        NavBar,
+        userDetail
     }
 }
 </script>
 
-<style>
+<style lang="less">
+    .userinfo {
+        .backimg{
+            height: 101px;
+            width: 100%;
+        }
 
+    }
 </style>
