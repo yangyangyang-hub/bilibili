05aaded966a9aee0a7296220e96dc8b1cc3a61ab
yangyangyang-hub <962369014@qq.com>
2020-11-30 09:46:45 +0800

    使用请求拦截和相应拦截处理token.3

--- bilibili/http.js
+++ bilibili/http.js
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import router from "./src/router"
+import Vue from 'vue'
 
 const http = axios.create({
     baseURL: 'http://112.74.99.5:3000/web/api'
@@ -16,6 +18,11 @@ http.interceptors.request.use(function (config) {
 http.interceptors.response.use(function (response) {
     return response;
   }, function (error) {
+    console.dir(error)
+    if (error.response.status == 401 || error.response.status == 402) {
+      router.push('/login')
+      Vue.prototype.$msg.fail(error.response.data.message)
+    }
     return Promise.reject(error);
   });
 
