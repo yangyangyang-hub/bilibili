f6e7d0f7c452f28608e141d8f792cca1482062b3
yangyangyang-hub <962369014@qq.com>
2020-12-01 19:22:24 +0800

    首页改造数据

--- bilibili/src/views/Home.vue
+++ bilibili/src/views/Home.vue
@@ -27,13 +27,36 @@ export default {
     async selectCategory() {
       const res = await this.$http.get('/category')
       this.changeCategory(res.data)
-      this.category = res.data
+     
     },
     changeCategory(data) {
       const category1 = data.map((item, index) => {
         item.list = []
+        item.page = 0
+        item.pagesize = 10
         return item
       })
+       this.category = category1
+    },
+    async selectArticle() {
+      const categoryitem = this.categoryItem()
+      const res = await this.$http.get('/detail/' + categoryitem._id, {
+        params: {
+          page:categoryitem.page,
+          pagesize: categoryitem.pagesize
+        }
+      })
+      categoryitem.list = res.data
+    },
+    categoryItem() {
+      const categoryitem = this.category[this.active]
+            console.log(222);
+      return categoryitem
+    }
+  },
+  watch: {
+    active() {
+      this.selectArticle()
     }
   },
   created() {
